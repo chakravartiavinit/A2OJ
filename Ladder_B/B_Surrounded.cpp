570a225af33273714472357269fcb078b4831b8d
kali666 <batsmans4u@gmail.com>
2022-01-13 05:08:45 +0530

    Solution

---
+++ Ladder_B/B_Surrounded.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+#pragma GCC optimization("O3")
+#pragma GCC optimization("unroll-loops")
+using namespace std;
+
+//LLLLLLLLLLL                               lllllll
+//L:::::::::L                               l:::::l
+//L:::::::::L                               l:::::l
+//LL:::::::LL                               l:::::l
+//  L:::::L               uuuuuu    uuuuuu   l::::l zzzzzzzzzzzzzzzzz    ssssssssss       eeeeeeeeeeee        cccccccccccccccc
+//  L:::::L               u::::u    u::::u   l::::l z:::::::::::::::z  ss::::::::::s    ee::::::::::::ee    cc:::::::::::::::c
+//  L:::::L               u::::u    u::::u   l::::l z::::::::::::::z ss:::::::::::::s  e::::::eeeee:::::ee c:::::::::::::::::c
+//  L:::::L               u::::u    u::::u   l::::l zzzzzzzz::::::z  s::::::ssss:::::se::::::e     e:::::ec:::::::cccccc:::::c
+//  L:::::L               u::::u    u::::u   l::::l       z::::::z    s:::::s  ssssss e:::::::eeeee::::::ec::::::c     ccccccc
+//  L:::::L               u::::u    u::::u   l::::l      z::::::z       s::::::s      e:::::::::::::::::e c:::::c
+//  L:::::L               u::::u    u::::u   l::::l     z::::::z           s::::::s   e::::::eeeeeeeeeee  c:::::c
+//  L:::::L         LLLLLLu:::::uuuu:::::u   l::::l    z::::::z      ssssss   s:::::s e:::::::e           c::::::c     ccccccc
+//LL:::::::LLLLLLLLL:::::Lu:::::::::::::::uul::::::l  z::::::zzzzzzzzs:::::ssss::::::se::::::::e          c:::::::cccccc:::::c
+//L::::::::::::::::::::::L u:::::::::::::::ul::::::l z::::::::::::::zs::::::::::::::s  e::::::::eeeeeeee   c:::::::::::::::::c
+//L::::::::::::::::::::::L  uu::::::::uu:::ul::::::lz:::::::::::::::z s:::::::::::ss    ee:::::::::::::e    cc:::::::::::::::c
+//LLLLLLLLLLLLLLLLLLLLLLLL    uuuuuuuu  uuuullllllllzzzzzzzzzzzzzzzzz  sssssssssss        eeeeeeeeeeeeee      cccccccccccccccc
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(long long t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+double Euclidean(double a, double b, double c, double d)
+{
+    double alpha = (a - c) * (a - c) + (b - d) * (b - d);
+    alpha = sqrt(alpha);
+    return alpha;
+}
+bool check_inside(double x1, double y1, double x2, double y2, double r1, double r2)
+{
+    double dist = Euclidean(x1, y1, x2, y2);
+    if (dist > (r1 + r2))
+    {
+        return true;
+    }
+    return false;
+}
+bool check_intersecting(double x1, double y1, double x2, double y2, double r1, double r2)
+{
+    double dist = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    double c1 = (r1 + r2) * (r1 + r2);
+    double c2 = (r1 - r2) * (r1 - r2);
+    if (c1 >= dist && dist > c2)
+    {
+        return true;
+    }
+    return false;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    double x1, y1, r1, x2, y2, r2;
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+    double dist = Euclidean(x1, y1, x2, y2);
+    double radius;
+    if (check_intersecting(x1, y1, x2, y2, r1, r2) == true)
+    {
+        cout << "0.0000000000\n";
+    }
+    else
+    {
+        if (check_inside(x1, y1, x2, y2, r1, r2) == true)
+        {
+            radius = (dist - r1 - r2) / 2;
+        }
+        else
+        {
+            if (r2 >= (dist + r1))
+            {
+                radius = (r2 - r1 - dist) / 2;
+            }
+            else
+            {
+                radius = (r1 - dist - r2) / 2;
+            }
+        }
+        cout << fixed << setprecision(15) << radius << "\n";
+    }
+}
