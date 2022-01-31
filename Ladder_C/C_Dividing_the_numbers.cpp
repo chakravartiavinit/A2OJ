2274733608fdb818d3ba6396634d1c082f1e84bc
kali666 <batsmans4u@gmail.com>
2022-01-31 14:01:42 +0530

    Solution

---
+++ Ladder_C/C_Dividing_the_numbers.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+#pragma GCC target("avx2")
+#pragma GCC optimization("O3")
+#pragma GCC optimization("unroll-loops")
+using namespace std;
+
+//LLLLLLLLLLL                               lllllll
+//L:::::::::L                               l:::::l
+//L:::::::::L                               l:::::l
+//LL:::::::LL                               l:::::l
+//  L:::::L               uuuuuu    uuuuuu   l::::l zzzzzzzzzzzzzzzzz    ssssssssss       eeeeeeeeeeee        cccccccccccccccc
+//  L:::::L               u::::u    u::::u   l::::l z:::::::::::::::z  ss::::::::::s    ee::::::::::::ee    cc:::::::::::::::c
+//  L:::::L               u::::u    u::::u   l::::l z::::::::::::::z ss:::::::::::::s  e::::::eeeee:::::ee c:::::::::::::::::c
+//  L:::::L               u::::u    u::::u   l::::l zzzzzzzz::::::z  s::::::ssss:::::se::::::e     e:::::ec:::::::cccccc:::::c
+//  L:::::L               u::::u    u::::u   l::::l       z::::::z    s:::::s  ssssss e:::::::eeeee::::::ec::::::c     ccccccc
+//  L:::::L               u::::u    u::::u   l::::l      z::::::z       s::::::s      e:::::::::::::::::e c:::::c
+//  L:::::L               u::::u    u::::u   l::::l     z::::::z           s::::::s   e::::::eeeeeeeeeee  c:::::c
+//  L:::::L         LLLLLLu:::::uuuu:::::u   l::::l    z::::::z      ssssss   s:::::s e:::::::e           c::::::c     ccccccc
+//LL:::::::LLLLLLLLL:::::Lu:::::::::::::::uul::::::l  z::::::zzzzzzzzs:::::ssss::::::se::::::::e          c:::::::cccccc:::::c
+//L::::::::::::::::::::::L u:::::::::::::::ul::::::l z::::::::::::::zs::::::::::::::s  e::::::::eeeeeeee   c:::::::::::::::::c
+//L::::::::::::::::::::::L  uu::::::::uu:::ul::::::lz:::::::::::::::z s:::::::::::ss    ee:::::::::::::e    cc:::::::::::::::c
+//LLLLLLLLLLLLLLLLLLLLLLLL    uuuuuuuu  uuuullllllllzzzzzzzzzzzzzzzzz  sssssssssss        eeeeeeeeeeeeee      cccccccccccccccc
+#define MOD 1000000007
+#define PI 3.141592653589793238462
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+/*########################################### DEBUG ###############################################################################################*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(long long t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+/*###############################################################################################################################################*/
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long n;
+    cin >> n;
+    set<int> st1, st2;
+    while (n >= 4)
+    {
+        st1.insert(n);
+        st2.insert(n - 1);
+        st2.insert(n - 2);
+        st1.insert(n - 3);
+        n -= 4;
+    }
+    if (n == 3)
+    {
+        st1.insert(3);
+        st2.insert(1);
+        st2.insert(2);
+    }
+    else if (n == 2)
+    {
+        st1.insert(1);
+        st2.insert(2);
+    }
+    else if (n == 1)
+    {
+        st1.insert(1);
+    }
+    long long sum = 0;
+    for (auto x : st1)
+    {
+        sum += x;
+    }
+    for (auto x : st2)
+    {
+        sum -= x;
+    }
+    cout << abs(sum) << "\n";
+    cout << st1.size() << " ";
+    for (auto x : st1)
+    {
+        cout << x << " ";
+    }
+}
